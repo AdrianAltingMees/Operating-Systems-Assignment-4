964dab77d85019942258c5c2b0e437f955ac6643
AdrianAltingMees <84096995+AdrianAltingMees@users.noreply.github.com>
2022-04-02 14:04:19 -0400

    Create q1.c

---
+++ q1.c
@@ -0,0 +1,16 @@
+/*
+ * Question1.c
+ * Author: Karan Singh, Adrian Alting-Mees
+ * ID: 190938920, 190743560
+ * Email: sing3892@mylaurier.ca, alti3560@mylaurier.ca
+ * Repo:
+ */
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <semaphore.h>
+#include <stdbool.h>
