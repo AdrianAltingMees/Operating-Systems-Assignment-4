219cd0bee7bb3386498360e88579fb30430b4678
AdrianAltingMees <84096995+AdrianAltingMees@users.noreply.github.com>
2022-04-02 14:04:45 -0400

    Update q1.c

--- q1.c
+++ q1.c
@@ -3,7 +3,7 @@
  * Author: Karan Singh, Adrian Alting-Mees
  * ID: 190938920, 190743560
  * Email: sing3892@mylaurier.ca, alti3560@mylaurier.ca
- * Repo:
+ * Repo: https://github.com/AdrianAltingMees/Operating-Systems-Assignment-4
  */
 #include <unistd.h>
 #include <stdio.h>
